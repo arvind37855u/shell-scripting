49d4f0f12bf231b2174ba530b21815621353a7b2
akg <akg@gmail.com>
2023-12-13 21:36:20 +0530

    created new file

---
+++ 01-shebang.sh
@@ -0,0 +1,20 @@
+#!/bin/shell
+
+# Anything that starts with # is referred as a comment.
+# Comments are just for the purpose of humans to make to more verbose.
+# Shebang Notation
+
+# Shebang Notation : This tells the script with which interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first line of the script.
+
+# Apart from first line if you see anyline that starts with # that's referred as a comment. 
+
+echo Welcome To Shell Scripting
+echo "This is day 1 of our shell scripting"
+
+echo Line1
+echo Line2
+echo Line3 
+
+echo Line4
+echo Line5
